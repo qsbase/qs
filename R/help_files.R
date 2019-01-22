a767f1a3081bcb7ef252d0e4f6b657f895e0972b
traversc <traversc@gmail.com>
2019-01-21 17:15:56 -0800

    added documentation; revert to roxygen 6.01

--- R/help_files.R
+++ R/help_files.R
@@ -42,7 +42,7 @@ NULL
 #' @param file the file name/path
 #' @return The de-serialized object
 #' @examples 
-#' x1 <- data.frame(int = sample(1e3, replace=TRUE), 
+#' x <- data.frame(int = sample(1e3, replace=TRUE), 
 #'                  num = rnorm(1e3), 
 #'                  char = randomStrings(1e3), stringsAsFactors = FALSE)
 #' myfile <- tempfile()
