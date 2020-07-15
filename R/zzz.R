89c32d4f0835acdd062e44b037478f92d212f8fe
tching <traversc@gmail.com>
2020-07-14 17:20:50 -0700

    .

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("qs v0.22.1. See ChangeLog for update details.")
+  packageStartupMessage("qs v0.23.1. See ChangeLog for update details.")
 }
