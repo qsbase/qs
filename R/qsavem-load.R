e1672cf090a044dd9c155ea292e6b61fa43c6494
Salim B <salim@posteo.de>
2021-11-11 16:58:29 +0100

    Further overhaul roxygen2 documentation:
    - add file `R/qs-package.R` for `package-level documentation, cf. https://roxygen2.r-lib.org/articles/rd.html#packages
    
    - let roxygen2 autogenerate the `NAMESPACE` file and add missing `@export` tags wherever needed
    
    - remove `@usage` blocks, so they are autogenerated by roxygen2
    
    - arrange roxygen2 tags in "standard" order (no need to explicitly name `@details` tag that way)
    
    - tweak documentation at various places
    
    Plus
    
    - harmonize R file extensions (`ascii_encoding.R`)
    
    - remove surplus trailing newlines

--- R/qsavem-load.R
+++ R/qsavem-load.R
@@ -1,17 +1,20 @@
 #' qsavem
-#' 
-#' Saves (serializes) multiple objects to disk. 
-#' @usage qsavem(...)
-#' @param ... objects to serialize. Named arguments will be passed to qsave during saving. Un-named arguments will be saved. A named file argument is required. 
-#' @details 
-#' This function extends qsave to replicate the functionality of base::save to save multiple objects. Read them back with qload.
-#' @examples 
-#' x1 <- data.frame(int = sample(1e3, replace=TRUE), 
-#'                  num = rnorm(1e3), 
+#'
+#' Saves (serializes) multiple objects to disk.
+#'
+#' This function extends [qsave()] to replicate the functionality of [base::save()] to save multiple objects. Read them back with [qload()].
+#'
+#' @param ... Objects to serialize. Named arguments will be passed to [qsave()] during saving. Un-named arguments will be saved. A named `file` argument is required.
+#'
+#' @export
+#'
+#' @examples
+#' x1 <- data.frame(int = sample(1e3, replace=TRUE),
+#'                  num = rnorm(1e3),
 #'                  char = sample(starnames$`IAU Name`, 1e3, replace=TRUE),
 #'                  stringsAsFactors = FALSE)
-#' x2 <- data.frame(int = sample(1e3, replace=TRUE), 
-#'                  num = rnorm(1e3), 
+#' x2 <- data.frame(int = sample(1e3, replace=TRUE),
+#'                  num = rnorm(1e3),
 #'                  char = sample(starnames$`IAU Name`, 1e3, replace=TRUE),
 #'                  stringsAsFactors = FALSE)
 #' myfile <- tempfile()
@@ -19,13 +22,12 @@
 #' rm(x1, x2)
 #' qload(myfile)
 #' exists('x1') && exists('x2') # returns true
-#' 
+#'
 #' # qs support multithreading
 #' qsavem(x1, x2, file=myfile, nthreads=2)
 #' rm(x1, x2)
 #' qload(myfile, nthreads=2)
 #' exists('x1') && exists('x2') # returns true
-#' @export
 qsavem <- function (...) {
   full_call <- as.list(sys.call())[-1]
   objects <- list(...)
@@ -39,22 +41,25 @@ qsavem <- function (...) {
 
 
 #' qload
-#' 
-#' Reads an object in a file serialized to disk using qsavem.
-#' @usage qload(file, env = parent.frame(), ...)
-#' @param file the file name/path.
-#' @param env the environment where the data should be loaded.
+#'
+#' Reads an object in a file serialized to disk using [qsavem()].
+#'
+#' This function extends qread to replicate the functionality of [base::load()] to load multiple saved objects into your workspace. `qload` and `qreadm` are alias of the same function.
+#'
+#' @param file The file name/path.
+#' @param env The environment where the data should be loaded.
 #' @param ... additional arguments will be passed to qread.
+#'
 #' @return Nothing is explicitly returned, but the function will load the saved objects into the workspace.
-#' @details 
-#' This function extends qread to replicate the functionality of base::load to load multiple saved objects into your workspace. `qload` and `qreadm` are alias of the same function.
-#' @examples 
-#' x1 <- data.frame(int = sample(1e3, replace=TRUE), 
-#'                  num = rnorm(1e3), 
+#' @export
+#'
+#' @examples
+#' x1 <- data.frame(int = sample(1e3, replace=TRUE),
+#'                  num = rnorm(1e3),
 #'                  char = sample(starnames$`IAU Name`, 1e3, replace=TRUE),
 #'                  stringsAsFactors = FALSE)
-#' x2 <- data.frame(int = sample(1e3, replace=TRUE), 
-#'                  num = rnorm(1e3), 
+#' x2 <- data.frame(int = sample(1e3, replace=TRUE),
+#'                  num = rnorm(1e3),
 #'                  char = sample(starnames$`IAU Name`, 1e3, replace=TRUE),
 #'                  stringsAsFactors = FALSE)
 #' myfile <- tempfile()
@@ -62,13 +67,12 @@ qsavem <- function (...) {
 #' rm(x1, x2)
 #' qload(myfile)
 #' exists('x1') && exists('x2') # returns true
-#' 
+#'
 #' # qs support multithreading
 #' qsavem(x1, x2, file=myfile, nthreads=2)
 #' rm(x1, x2)
 #' qload(myfile, nthreads=2)
 #' exists('x1') && exists('x2') # returns true
-#' @export
 qreadm <- function(file, env = parent.frame(), ...) {
   
   savelist <- qread(file, ...)
@@ -81,7 +85,7 @@ qreadm <- function(file, env = parent.frame(), ...) {
 
 
 #' qreadm
-#' @usage qreadm(file, env = parent.frame(), ...)
+#'
 #' @rdname qreadm
 #' @export
 qload <- qreadm
